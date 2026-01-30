9763c17cfa2238007cc3f3ccac7a8a7279a6625c
mahmoodharis540 <c4017640@hallam.shu.ac.uk>
2026-01-30 14:45:03 +0000

    dummypages

--- nhs-finder/app/homepage/page.tsx
+++ nhs-finder/app/homepage/page.tsx
@@ -1,9 +1,5 @@
 export default function HomePage() {
-    const listData = [
-        { title: "Jessops Wing A", content: "Details about Building A" },
-        { title: "Radiology B", content: "Details about Building B" },
-        { title: "AI Building C", content: "Details about Building C" },
-      ];
+    
   return (
     <main className='max-w-2xl mx-auto py-10'>
       <h1>Welcome to NHS Finder</h1>
