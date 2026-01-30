3099748e2751e097a90b435c4ab05835bbe8c8d5
mahmoodharis540 <c4017640@hallam.shu.ac.uk>
2026-01-30 15:19:31 +0000

    MainPage created dummypage #2

--- nhs-finder/app/layout.tsx
+++ nhs-finder/app/layout.tsx
@@ -24,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className = "bg-[#003087] text-white min-h-screen">
         {children}
       </body>
     </html>
