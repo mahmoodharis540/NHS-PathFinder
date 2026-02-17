1953b0ac38b8875e14812ef1d9877979ce6bc3b1
jack <71027978+Jack01110@users.noreply.github.com>
2026-02-17 12:52:35 +0000

    fixed now

--- nhs-finder/next-intl.config.js
+++ nhs-finder/next-intl.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-intl').NextIntlConfig} */
 module.exports = {
   locales: ["en", "es","pl","fr"], // supported languages got rid of , "fr", "pl", "pa", "ur", "bn", "gu", "ar", "zh"
-  defaultLocale: "en", // fallback if no locale is provided
+  defaultLocale: "es", // fallback if no locale is provided
   messagesDirectory: "messages", // folder where your translation JSON files live
 };
