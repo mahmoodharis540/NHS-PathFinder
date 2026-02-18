492d76744fa2bd59d32f68212a562e0123ceb7a9
mahmoodharis540 <c4017640@hallam.shu.ac.uk>
2026-02-18 14:25:12 +0000

    File uploads now added

--- nhs-finder/lib/prisma.ts
+++ nhs-finder/lib/prisma.ts
@@ -1,9 +1,21 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as any;
+// const globalForPrisma = globalThis as any;
+
+// export const prisma =
+//   globalForPrisma.prisma ?? new PrismaClient();
+
+// if (process.env.NODE_ENV !== 'production')
+//   globalForPrisma.prisma = prisma;
+
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== 'production')
-  globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
