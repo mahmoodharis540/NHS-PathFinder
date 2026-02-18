f84e37c06c9e75be30a6c162e86ed649b3f79830
mahmoodharis540 <c4017640@hallam.shu.ac.uk>
2026-02-18 23:59:40 +0000

    translation addition

--- nhs-finder/components/TopBar.tsx
+++ nhs-finder/components/TopBar.tsx
@@ -1,26 +1,3 @@
-// "use client";
-
-// import Link from "next/link";
-// import { Settings } from "lucide-react";
-// 0
-// export default function TopBar() {
-//   return (
-//     <header className="absolute top-0 left-0 w-full p-4 flex items-center justify-between">
-//       {/* Left side */}
-//       <Link
-//         href="\login"
-//         className="text-white border border-white px-4 py-1 rounded hover:bg-white hover:text-[#003087] transition"
-//       >
-//         Staff Portal
-//       </Link>
-
-//       {/* Right side */}
-//       <Link href="/settings" className="text-white hover:opacity-80">
-//         <Settings className="h-6 w-6" />
-//       </Link>
-//     </header>
-//   );
-// }
 "use client";
 
 import Link from "next/link";
