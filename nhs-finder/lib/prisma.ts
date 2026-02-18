d5dfab9d44d97e04fd5db21170a7d98917fe93c2
jack <71027978+Jack01110@users.noreply.github.com>
2026-02-18 11:40:28 +0000

    fix? i think?

--- nhs-finder/lib/prisma.ts
+++ nhs-finder/lib/prisma.ts
@@ -1,10 +1,9 @@
-import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = globalThis as any;
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
-export { prisma };
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.prisma = prisma;
